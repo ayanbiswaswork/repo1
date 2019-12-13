02a54aafe781d1fbf7daef9506041194de7ab6fc
ayanbiswaswork <58822391+ayanbiswaswork@users.noreply.github.com>
2019-12-13 22:47:23 +0530

    1st manual

--- HelllWorld1.java
+++ HelllWorld1.java
@@ -4,6 +4,6 @@ class HelloWorld1
   public static void main (String args[] )
                 {
 
-                 System.out.println("-----------------------1-----------------------");
+                 System.out.println("-----------------------1 2 3-----------------------");
                 }
 }
